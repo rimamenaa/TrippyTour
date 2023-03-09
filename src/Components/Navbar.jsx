033e49c3d2cd0a4a7d353745e9089f273c34b4b5
rimamenaa <rimamenaa98@gmail.com>
2023-03-09 21:50:13 +0100

    responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,23 +3,31 @@ import { MenuItems } from "./MenuItems";
 import "./NavbarStyles.css";
 
 export class Navbar extends Component {
+  state = {clicked: false};
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  }
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Trippy</h1>
+        
+        <div className="menu-icons" onClick={ this.handleClick }>
+        <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
 
         <ul className="nav-menu">
           {MenuItems.map((item, i) => {
             return (
               <li key={i}>
-                <a href="/">
+                <a className={item.cName} href="/">
                   <i className={item.icon}></i>
                   {item.title}
                 </a>
               </li>
             )
           })}
-
+          <button>Sign Up</button>
         </ul>
 
       </nav>
